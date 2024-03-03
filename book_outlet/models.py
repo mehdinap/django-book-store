e25c45588caadf863a18d1945121b73569338295
user <git@ganj.yaftyar.ir:najibpour_m>
2024-03-03 20:04:52 +0330

    foinish models and come to admin

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,25 @@
 from django.db import models
+from django.core.validators import (
+    MinValueValidator as minval,
+    MaxValueValidator as maxval,
+)
+from django.urls import reverse
+from django.utils.text import slugify
 
 class Book(models.Model):
-    title=models.CharField(max_length=50)
-    rating=models.IntegerField()
-    
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField(validators=[minval(1), maxval(5)])
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", blank=True,null=False,db_index=True)
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.slug])
+
+    def save(self,*args,**kwards):
+        self.slug=slugify(self.title)
+        super().save(*args,**kwards)
+
+
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import Http404
+from django.db.models import Avg
+from .models import Book
 
 # Create your views here.
+
+
+def index(request):
+    books = Book.objects.all().order_by("-title")
+    num_books = books.count()
+    avg=books.aggregate(Avg("rating"))
+    return render(request, "book_outlet/index.html", {
+        "books": books,
+        "total_number_of_books":num_books,
+        'avg':avg
+        })
+
+
+def book_detail(request,slug):
+    # try:
+    #     book = Book.objects.get(pk=id)
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book,slug=slug)
+    return render(request,"book_outlet/book_detail.html",{
+        'title':book.title,
+        'author':book.author,
+        'rating':book.rating,
+        'is_bestseller':book.is_bestselling
+    })
